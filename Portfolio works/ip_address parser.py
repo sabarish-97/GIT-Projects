84c728a6ee3238fb21573258d3c2728490007020
sabarish-97 <sabarish_97@yahoo.com>
2024-02-17 13:53:29 +0000

    1st prototype

--- Portfolio works/ip_address parser.py
+++ Portfolio works/ip_address parser.py
@@ -7,5 +7,16 @@ def file_update(import_file, remove_list):    # import file consists of allowed 
         text_list = text.split()      # converting text to list
 
         for element in text_list:
-            if element in remove_list
+            if element in remove_list:
+                text_list.remove(element)
+    
+        text = " ".join(text_list)
+    with open(import_file, "w") as file:
+        file.write(text)
+
+file_update("allow_list.txt", ["192.168.25.60", "192.168.140.81", "192.168.203.198"])
+
+with open("allow.txt", "r") as file:
+    text1 = file.read()
+print(text1)
             
