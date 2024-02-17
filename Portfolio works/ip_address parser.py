4adbeac8f8b02b6504fb977b626a8678952400dd
sabarish-97 <sabarish_97@yahoo.com>
2024-02-17 16:00:11 +0000

    prototype successful

--- Portfolio works/ip_address parser.py
+++ Portfolio works/ip_address parser.py
@@ -3,8 +3,10 @@ import re
 def file_update(import_file, remove_list):    # import file consists of allowed IP addresses
     with open(import_file, "r") as file:
         text = file.read()
-        print(f"these are the existing allowed IP address {text}")
         text_list = text.split()              # converting string to list
+        print(f"\nthese are the existing allowed IP address\n")
+        for i in text_list:                   # printing the IP Address line by line
+            print(f"{i}")
 
         for element in text_list:             # iteration to check every IP address in the list
             if element in remove_list:
@@ -14,11 +16,11 @@ def file_update(import_file, remove_list):    # import file consists of allowed 
     with open(import_file, "w") as file:
         file.write(text)                      # update the file 
 
-file_update("allow_list.txt", ["192.168.6.9", "192.168.140.81", "192.168.203.198"])
+file_update("allow_list.txt", ["192.168.52.90", "192.168.140.81", "192.168.203.198"])
 
 with open("allow_list.txt", "r") as file:     # read the contents of the file
     text1 = file.read()
-print(f"please find the updated IP Address details that are allowed")
+print(f"\nplease find the updated IP Address details that are allowed\n")
 pattern = "\d+\.\d+\.\d+\.\d+\d"
 separated_string = re.findall(pattern, text1) # using regex to split individual IP address
 for i in separated_string:                    # using for loop to print IP Address line by line
