800b4715a5851f3f1c98bcdb2054fe2808bdca48
sabarish-97 <sabarish_97@yahoo.com>
2024-02-18 11:30:26 +0000

    commit

---
+++ Lecture tasks/list task.py
@@ -0,0 +1,9 @@
+city_list = ["Glasgow", "London", "Edinburgh"]
+print (city_list[2])
+print (city_list[1], city_list[2])
+city_list.append("Manchester")
+print(city_list)
+city_list[1] = "Birmingham"
+print(city_list)
+for i in city_list:
+    print(i)
