4504f52658e4afbd690cdfa9ab26dc3dd2f2c3e0
sabarish-97 <sabarish_97@yahoo.com>
2024-02-18 11:38:13 +0000

    commit

---
+++ Lecture tasks/L2 user input age.py
@@ -0,0 +1,8 @@
+age = int(input("please enter your age"))
+if age < 18:
+    print("you are a minor")
+elif age <= 65 and age >=18:
+    print("you are an adult")
+elif age > 65:
+    print("you are a senior citizen")
+     
