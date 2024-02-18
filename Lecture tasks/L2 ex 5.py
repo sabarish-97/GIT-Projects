de0a5c30dfae5e5cc633bb722e96f91f42cec634
sabarish-97 <sabarish_97@yahoo.com>
2024-02-18 11:29:02 +0000

    commit

---
+++ Lecture tasks/L2 ex 5.py
@@ -0,0 +1,9 @@
+grade = 55 
+if grade < 50: 
+    print("You failed") 
+elif grade < 60: 
+    print("You passed") 
+elif grade < 70: 
+    print("You got a good pass") 
+else: 
+    print("You got an excellent pass") 
