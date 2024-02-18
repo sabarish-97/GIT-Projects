0d484136d763ffafe3570613c27658a6f3119e72
sabarish-97 <sabarish_97@yahoo.com>
2024-02-18 11:31:16 +0000

    commit

---
+++ Lecture tasks/L2 summary task 2.py
@@ -0,0 +1,10 @@
+colours = ["red", "blue", "green"]
+print(colours)
+print(colours[1])
+colours[0] = "cyan"
+print(colours)
+print(len(colours))
+if "red" in colours:
+    print("red is in the list")
+selected_colours = [colours[1], colours[2]]
+print(selected_colours)
