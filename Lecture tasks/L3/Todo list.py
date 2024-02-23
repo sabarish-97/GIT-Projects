ebbfd8adf8d9e112d0ec7d1de2233735159e359c
sabarish-97 <sabarish_97@yahoo.com>
2024-02-23 15:34:16 +0000

    second commit

--- Lecture tasks/L3/Todo list.py
+++ Lecture tasks/L3/Todo list.py
@@ -3,4 +3,36 @@ print("1. Add a new task\n2. View the current tasks in the list\n3. Remove a tas
 def add_task(taskaddstring):
     list_init.append(taskaddstring)
 
-def
+def view_task():
+    print(f"{list_init}")
+
+def remove_task():
+    print(f"please select which task you want to remove from the list{list_init}")
+    rem1 = int(input)
+    list_init.pop(rem1)
+    print(f"The updated list is as follows {list_init}")
+
+def exit_program():
+    print("thank you for using the program, Program exited")
+    quit()
+
+get_input = int(input("please select an option to proceed"))
+
+if get_input == 1:
+    new_task = str(input("please enter a new task"))
+    add_task(new_task)
+
+elif get_input == 2:
+    view_task()
+
+elif get_input == 3:
+    remove_task()
+
+elif get_input == 4:
+    exit_program()
+
+else:
+    print("\nplease select a valid selection")
+
+
+    
