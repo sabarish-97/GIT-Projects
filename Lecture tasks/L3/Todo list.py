3e3f1b58bed0159025c3cee3f38758ab1db68260
sabarish-97 <sabarish_97@yahoo.com>
2024-02-23 13:35:09 +0000

    commit

---
+++ Lecture tasks/L3/Todo list.py
@@ -0,0 +1,6 @@
+list_init = [""]
+print("1. Add a new task\n2. View the current tasks in the list\n3. Remove a task from the list\n4. Quit and exit the program ")
+def add_task(taskaddstring):
+    list_init.append(taskaddstring)
+
+def
