84e6b88061ec0d0e61a196b4e1f5156424a291d8
sabarish-97 <sabarish_97@yahoo.com>
2024-02-26 14:18:44 +0000

    added comments

--- Lecture tasks/L3/Todo list.py
+++ Lecture tasks/L3/Todo list.py
@@ -1,13 +1,18 @@
 
+## defining functions
+# adding a task
+
 def add_task(taskaddstring):
     list_init.append(taskaddstring)
-    
+
+# Viewing a task
 def view_task():
     print("----------------")
     for i, tasks in enumerate(list_init):
         print(i+1, tasks)
-    print("----------------")   
+    print("----------------")
 
+# Removing a Task
 def remove_task():
     print("----------------")
     for i, tasks in enumerate(list_init):
@@ -21,16 +26,20 @@ def remove_task():
         print(i+1, tasks)
     print("----------------")  
 
+
+# Exiting a program
 def exit_program():
     print("thank you for using the program, Program exited")
     quit()
-    
+
+# Gettting an Input from the user    
 def input_option():
     print("#####   TO-DO LIST   ######")
     print("1. Add a new task\n2. View the current tasks in the list\n3. Remove a task from the list\n4. Quit and exit the program ")
     input1 = int(input("please select an option to proceed\n"))
     return input1
 
+# Main Loop Function
 def loop_function():
     get_input = input_option()
     while get_input < 6:
@@ -57,6 +66,8 @@ def loop_function():
         else:
             print("\nplease select a valid selection")
             break
+
+# Main Program
 
 list_init = []
 loop_function()
