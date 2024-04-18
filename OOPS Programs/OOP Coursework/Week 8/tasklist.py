64443657366b64b263820ee784baf55f7e39ec8e
sabarish-97 <sabarish_97@yahoo.com>
2024-04-18 17:19:37 +0100

    minor change

--- OOPS Programs/OOP Coursework/Week 8/tasklist.py
+++ OOPS Programs/OOP Coursework/Week 8/tasklist.py
@@ -23,6 +23,6 @@ class TaskList:
         current_date = datetime.datetime.now()
         print("Overdue tasks:")
         for task in self.tasks:
-            if task.date_due < current_date and not task.completed:
+            if Task.date_due < current_date and not task.completed:
                 print(task)
 
